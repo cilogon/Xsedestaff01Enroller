cbf82f33fdc5812dc6aff5447c7ece9bf545f94f
Scott Koranda <skoranda@gmail.com>
2021-07-15 13:31:58 -0500

    Add all enum values to model validation rules
    Add all the necessary enum values to the models so that the
    validation rules pass.

--- Model/XsedestaffComputeAllocation.php
+++ Model/XsedestaffComputeAllocation.php
@@ -24,7 +24,16 @@ class XsedestaffComputeAllocation extends AppModel {
     'allocation' => array(
       'rule' => array('inList', array(XsedeStaffComputeAllocationEnum::TG_ASC160050,
                                       XsedeStaffComputeAllocationEnum::TG_ASC160051,
-                                      XsedeStaffComputeAllocationEnum::TG_ASC170016)),
+                                      XsedeStaffComputeAllocationEnum::TG_ASC170016,
+                                      XsedeStaffComputeAllocationEnum::TG_ASC170030,
+                                      XsedeStaffComputeAllocationEnum::TG_ASC170035,
+                                      XsedeStaffComputeAllocationEnum::TG_CDA170005,
+                                      XsedeStaffComputeAllocationEnum::TG_DDM16003,
+                                      XsedeStaffComputeAllocationEnum::TG_IRI160007,
+                                      XsedeStaffComputeAllocationEnum::TG_STA160002,
+                                      XsedeStaffComputeAllocationEnum::TG_STA160003,
+                                      XsedeStaffComputeAllocationEnum::TG_STA170001,
+                                      XsedeStaffComputeAllocationEnum::TG_TRA160027)),
       'required' => true,
       'allowEmpty' => false
     )
